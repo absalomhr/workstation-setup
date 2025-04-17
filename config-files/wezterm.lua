8421470c057cf7c444af7b90bd10fe2e7023ebc9
= <=>
2025-04-17 15:39:47 -0600

    key configs and font update

--- config-files/wezterm.lua
+++ config-files/wezterm.lua
@@ -11,8 +11,8 @@ config = {
   adjust_window_size_when_changing_font_size = false,
   window_decorations = "RESIZE",
   tab_bar_at_bottom = true,
-  font_size = 14.5,
-  font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+  font_size = 17.5,
+  font = wezterm.font("Inconsolata Nerd Font Mono", {weight="Bold", stretch="Normal", style="Normal"}),
   enable_tab_bar = true,
   window_padding = {
     left = 15,
@@ -86,4 +86,25 @@ config = {
     },
   },
 }
+
+config. keys = {
+
+  -- Alt + Left Arrow to move to the previous word
+
+  {key="LeftArrow", mods="ALT", action=wezterm.action{SendString="\x1bb"}},
+
+  -- Alt + Right Arrow to move to the next word
+
+  {key="RightArrow", mods="ALT", action=wezterm.action{SendString="\x1bf"}},
+
+  -- Cmd + Left Arrow to move to the beginning of the line
+
+  {key="LeftArrow", mods="CMD", action=wezterm.action{SendString="\x01"}},
+
+  -- Cmd + Right Arrow to move to the end of the line
+
+  {key="RightArrow", mods="CMD", action=wezterm.action{SendString="\x05"}},
+
+}
+
 return config
